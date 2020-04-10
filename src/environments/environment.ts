74030f221c625577bde45b441f375df824ec919c
Yuvi R <yuvaranjani.rajendran@gmail.com>
2020-04-10 16:21:58 -0500

    Added env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  movieInfoUrl: 'https://my-json-server.typicode.com/suren2k17/dummyAPI/movies'
 };
 
 /*
